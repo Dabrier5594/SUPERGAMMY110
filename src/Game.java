722ea97ccc99838d1e4ac5db2b84edf5f6c3149a
dabrier5594 <dabrier5594@student.pps.net>
2025-10-16 14:06:07 -0700

    ugg

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Game {
     static Scanner scanner = new Scanner(System.in);
 
-
-
     public static void main(String[] args) {
-
         // Create the initial room
         Hub cave = new Hub("Tom's Dark Cave", "The back of the ancient cave where Tom the hermit lived for many years.");
 
@@ -21,11 +19,46 @@ public class Game {
 
     public static void Story(Hub cave) {
 
-        // Creating start boolean
-        boolean start;
+        //List of verbs
+        List<String> verbs = new ArrayList<>();
+        verbs.add("move");
+        verbs.add("go");
+        verbs.add("look");
+        verbs.add("open");
+        verbs.add("drop");
+        verbs.add("take");
+        verbs.add("read");
+        verbs.add("push");
+        verbs.add("pull");
+        verbs.add("wait");
+        verbs.add("listen");
+        verbs.add("lock");
+        verbs.add("close");
+        verbs.add("n");
+        verbs.add("s");
+        verbs.add("w");
+        verbs.add("e");
+        verbs.add("north");
+        verbs.add("south");
+        verbs.add("west");
+        verbs.add("east");
+
+
+        List<String> objects = new ArrayList<>();
+        objects.add("w");
+        objects.add("e");
+        objects.add("s");
+        objects.add("n");
+        objects.add("west");
+        objects.add("east");
+        objects.add("south");
+        objects.add("north");
+
 
         // Makes the room you are in "cave"
         Hub inRoom = cave;
+
+        boolean start;
 
         // Asks if you want to play
         System.out.print("Play (yes or no)? \n-> ");
@@ -45,23 +78,60 @@ public class Game {
         }
 
         //While start is true, run the code
-        if (start){
+        while (start) {
+            System.out.print("-> ");
+            String action = scanner.nextLine();
 
-            while(true) {
-                System.out.print("-> ");
+            //checks to see if input has verb
+            if (verbs.contains(action)) {
 
-                String action = scanner.nextLine();
 
-                // If user said "look" then description of current room prints
-                if (action.toLowerCase().equals("look")) {
-                    System.out.println(inRoom.getRoomDescription());
-                }
 
-                // If user moves north update room (if possible)
-                if (action.toLowerCase().equals("n") || action.toLowerCase().equals("north")) {
-                    // Call the move method and update to inRoom
-                    inRoom = move(action.toLowerCase(), inRoom);
-                }
+                //checks to see if input has an object for the verb to act on if needed (open, close etc.)
+
+                    // If user said "look" then description of current room prints
+                    if (action.toLowerCase().equals("look")) {
+                        System.out.println(inRoom.getRoomDescription());
+                    }
+
+                    // If user moves north update room (if possible)
+                    if (action.toLowerCase().equals("n") || action.toLowerCase().equals("north")) {
+                        // Call the move method and update to inRoom
+                        inRoom = move(action.toLowerCase(), inRoom);
+
+                        System.out.println(inRoom.getRoomDescription());
+                    }
+
+                    if (action.toLowerCase().equals("s") || action.toLowerCase().equals("south")) {
+                        // Call the move method and update to inRoom
+                        inRoom = move(action.toLowerCase(), inRoom);
+
+                        System.out.println(inRoom.getRoomDescription());
+                    }
+
+                    if (action.toLowerCase().equals("w") || action.toLowerCase().equals("west")) {
+                        // Call the move method and update to inRoom
+                        inRoom = move(action.toLowerCase(), inRoom);
+
+                        System.out.println(inRoom.getRoomDescription());
+                    }
+
+                    if (action.toLowerCase().equals("e") || action.toLowerCase().equals("east")) {
+                        // Call the move method and update to inRoom
+                        inRoom = move(action.toLowerCase(), inRoom);
+
+                        System.out.println(inRoom.getRoomDescription());
+
+                    }
+            }
+
+            else if (){
+
+                //input does not contain a known word)
+            }
+
+            else {
+                System.out.println("You need a verb do something!");
             }
         }
     }
@@ -69,13 +139,27 @@ public class Game {
     // move method
     public static Hub move(String direction, Hub currentHub) {
 
+        if (direction.equals("north")) {
+            direction = "n";
+        }
+
+        if (direction.equals("south")) {
+            direction = "s";
+        }
+
+        if (direction.equals("west")) {
+            direction = "w";
+        }
+
+        if (direction.equals("east")) {
+            direction = "e";
+        }
         Hub newRoom = currentHub.getExit(direction);
 
         // If there is a new area where user tries to go, update it
         if (newRoom != null) {
 
-            System.out.println("You move " + direction + ".");
-            return newRoom; // Return the new Hub object
+            return newRoom; // Return the new Hub object (room)
 
         }
 
